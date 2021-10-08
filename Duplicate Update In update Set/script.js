ce6dc69658098f9375e9be2cc5e93be414def05d
ashu759 <33026500+ashu759@users.noreply.github.com>
2021-10-08 23:23:50 +0200

    Duplicate updates in same update set

--- Duplicate Update In update Set/script.js
+++ Duplicate Update In update Set/script.js
@@ -1,10 +1,10 @@
-(function(findings, updatexml, current, updateset) {
+(function(findings, updatexml, updateset) {
 
     var sysUpdateXmlGr = new GlideRecord('sys_update_xml');
     sysUpdateXmlGr.addQuery('name', updatexml.getValue('name'));
     sysUpdateXmlGr.addQuery('update_set', updateset.getValue('sys_id'));
     sysUpdateXmlGr.addQuery('sys_id', '!=', updatexml.getValue('sys_id'));
-	  sysUpdateXmlGr.setLimit(1);
+    sysUpdateXmlGr.setLimit(1);
     sysUpdateXmlGr.query();
     if(sysUpdateXmlGr.hasNext()){
        findings.increment();
